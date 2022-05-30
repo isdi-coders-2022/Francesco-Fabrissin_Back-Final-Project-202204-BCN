0bf5c31e901b2f85940f26f25f01e0229893777f
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-05-30 19:36:32 +0200

    created joi validation schema and added validation to the chain

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const { validate } = require("express-validation");
 const userLogin = require("../controllers/userControllers");
+const {
+  credentialsLoginSchema,
+} = require("../server/schemas/usersCredentialsSchemas");
 
 const userRouter = express.Router();
 
-userRouter.post("/login", userLogin);
+userRouter.post("/login", validate(credentialsLoginSchema), userLogin);
 
 module.exports = userRouter;
