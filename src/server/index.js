ee51cb6797cdbc4ff42efe000a56af82307ac7f2
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-05-30 17:22:25 +0200

    created userRouter

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const { notFoundError, generalError } = require("./middlewares/errors");
+const userRouter = require("../routers/userRouter");
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+
+app.use("/user", userRouter);
 
 app.use(notFoundError);
 app.use(generalError);
