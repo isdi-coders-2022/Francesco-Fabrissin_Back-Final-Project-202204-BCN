177ad7290a8a1a1c7e4aae900aef8d7a767d4245
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-05-30 22:04:18 +0200

    tested userRuter Login path

---
+++ src/routers/userRouter.test.js
@@ -0,0 +1,40 @@
+const { default: mongoose } = require("mongoose");
+const request = require("supertest");
+const connectDB = require("../database");
+const User = require("../database/models/User");
+const mockUsers = require("../mocks/mockUsers");
+const app = require("../server/index");
+
+beforeAll(async () => {
+  await connectDB(process.env.MONGO_CONNECTION_TEST);
+});
+
+beforeEach(async () => {
+  await User.create(mockUsers[0]);
+  await User.create(mockUsers[1]);
+});
+
+afterEach(async () => {
+  await User.deleteMany({});
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+describe("Given a POST/user/login endpoint", () => {
+  describe("When it receives a request with a users present in the database", () => {
+    test("Then it should respond with a 200 status and a token", async () => {
+      const user = {
+        username: "fra432",
+        password: "fra432",
+      };
+
+      const {
+        body: { token },
+      } = await request(app).post("/user/login").send(user).expect(200);
+
+      expect(token).not.toBeNull();
+    });
+  });
+});
