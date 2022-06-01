85cfd1ab8f86703182636b25c80b68a71bfb8920
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-01 13:07:47 +0200

    tested bad request path for userRegister integration test

--- src/routers/userRouter.test.js
+++ src/routers/userRouter.test.js
@@ -62,7 +62,29 @@ describe("Given a POST/user/login endpoint", () => {
 });
 
 describe("Given a POST/user/register endpoint", () => {
-  describe("When it receives a request with a users present in the database", () => {
+  describe("When it receives a request with wrong user credentials", () => {
+    test("Then it should respond with a 400 status and a 'Bad request error message'", async () => {
+      const expectedErrorMessage = "Bad request";
+      const testFile = "test file";
+
+      const {
+        body: { message },
+      } = await request(app)
+        .post("/user/register")
+        .type("multipart/form-data")
+        .field("username", "sergio")
+        .field("password", "sergio")
+        .attach("image", Buffer.from(testFile, "utf-8"), {
+          filename: "12798217782",
+          originalname: "image.jpg",
+        })
+        .expect(400);
+
+      expect(message).toBe(expectedErrorMessage);
+    });
+  });
+
+  describe("When it receives a request with a users not present in the database", () => {
     test("Then it should respond with a 200 status and a token", async () => {
       const testFile = "test file";
 
