e8d2a9520b5bed72dbb6722012db75ebafb2502f
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-04 00:13:09 +0200

    added express static to middlewares chain

--- src/server/index.js
+++ src/server/index.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
+app.use(express.static("uploads"));
 app.use(express.json());
 
 app.use("/user", userRouter);
