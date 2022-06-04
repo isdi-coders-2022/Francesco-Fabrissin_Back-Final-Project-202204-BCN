31c3984c6d7820dfd389dba49dea1fb825148bb8
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-04 19:48:32 +0200

    remved unused import

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const helmet = require("helmet");
 const { notFoundError, generalError } = require("./middlewares/errors");
 const userRouter = require("../routers/userRouter");
 const usersRouter = require("../routers/usersRouter");
