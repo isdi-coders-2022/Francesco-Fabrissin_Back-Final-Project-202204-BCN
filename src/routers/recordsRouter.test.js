b41d3f930105ed1213797f9325a61b1ec1fc9c3f
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-06 12:17:45 +0200

    tested recordsRouter, delte record path

--- src/routers/recordsRouter.test.js
+++ src/routers/recordsRouter.test.js
@@ -69,10 +69,6 @@ describe("Given a GET/myCollection endpoint", () => {
 describe("Given a DELETE/myCollection/:recordId endpoint", () => {
   describe("When it receives a request with a record id", () => {
     test("Then it should respond with a status 200 and a deleted record", async () => {
-      const user = {
-        username: "fra433",
-        password: "fra432",
-      };
       const record = await Record.find({ title: "Brace & Bit" });
       const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOTYwOTk5MzliYjlkZmFkOWY5OTI5NyIsInVzZXJuYW1lIjoiZnJhNDMyIiwiaW1hZ2UiOiJodHRwczovL3ZpZGVvLWVjdjEtMS54eC5mYmNkbi5uZXQvdi90MS42NDM1LTkvMzc4Njc2NDNfMjE4MTc5MDEyODc3MjA3OV8zNDk2Njg1NjkwNjA5OTkxNjgwX24uanBnP19uY19jYXQ9MTA4JmNjYj0xLTcmX25jX3NpZD0wOWNiZmUmX25jX29oYz1MWFNxN1F1RFpnUUFYXzF0NzBaJl9uY19odD12aWRlby1lY3YxLTEueHgmb2g9MDBfQVQtWXRoLW9PaUJZNXRZVVVoX29udlpFZnhGRTlIMWRxd1V6ejc1ZDVZc1VEdyZvZT02MkJDRTk2QSIsImlhdCI6MTY1NDUxMDQ3NX0.jJpqlaW_kflpVjuvS86EuVqqlS93Y2D8IC-axYFa7jg";
